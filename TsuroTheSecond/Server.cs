7d05d111278f166f8b7f6a86b83211c54a7f8478
ethlee4 <ethanlee2020@u.northwestern.edu>
2018-04-23 19:31:19 -0500

    oops pushed bad code

--- TsuroTheSecond/Server.cs
+++ TsuroTheSecond/Server.cs
@@ -12,13 +12,13 @@ namespace TsuroTheSecond
         List<SPlayer> dead;
         List<Tile> deck;
         Board board;
-        List<String> colors = new List<string>{"blue", "green", "black", "yellow", "red", "orange", "white", "pink"}
+        List<String> colors = new List<string> { "blue", "green", "black", "yellow", "red", "orange", "white", "pink" };
                
         public Server(int playerCount) {
             // initializes the game
             alive = new List<SPlayer>();
             dead = new List<SPlayer>();
-            board = new Board();
+            board = new Board(6);
 
             for (int i = 0; i < playerCount; i++) {
                 alive.Add(new SPlayer(colors[i]));
