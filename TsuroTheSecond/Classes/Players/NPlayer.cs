d5101a9d301440f155e1971d16ec7dc36d74e5ef
mattengelken <matt.engelken@gmail.com>
2018-05-30 01:58:31 -0500

    finished nplayer

--- TsuroTheSecond/Classes/Players/NPlayer.cs
+++ TsuroTheSecond/Classes/Players/NPlayer.cs
@@ -8,101 +8,107 @@ namespace TsuroTheSecond
 {
     public class NPlayer : IPlayer
     {
-        string color;
-        string name;
-        List<string> allColors;
         ToXml toXml;
         Parser parser;
         NetworkStream stream;
-        byte[] readBuffer;
-        byte[] writeBuffer;
-        string inputString;
-        string outputString;
+
 
         public NPlayer(NetworkStream s)
         {
-            
             toXml = new ToXml();
             parser = new Parser();
-            stream = s;
-            readBuffer = new byte[1024];
-            writeBuffer = new byte[1024];
-            inputString = "";
-            outputString = "";
+            stream = s;  
         }
 
         public string GetName() {
-            inputString = "<get-name></get-name>";
-            writeBuffer = Encoding.ASCII.GetBytes(inputString);
-            stream.Write(writeBuffer, 0, writeBuffer.Length);
-            int numberOfBytesRead = 0;
-            StringBuilder myCompleteMessage = new StringBuilder();
-            // Incoming message may be larger than the buffer size.
-            do
-            {
-                numberOfBytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
+            string getname = "<get-name></get-name>";
 
-                myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(readBuffer, 0, numberOfBytesRead));
+            string response = byteStreamHelper(getname);
 
-            }
-            while (stream.DataAvailable);
-            return parser.PlayerNameParse(myCompleteMessage);
-
+            return parser.PlayerNameParse(response);
         }
 
-        public void Initialize(string _color, List<string> _allColors) {
+
+        public void Initialize(string _color, List<string> _allColors)
+        {
             XElement InitXml = toXml.InitializetoXml(_color, _allColors);
             string init = toXml.FormatXml(InitXml);
 
-            // server is sending this
-            Console.WriteLine(init);
-            
-
-            // receiving back this
-            string response = Console.ReadLine();
-      
+            string response = byteStreamHelper(init);
 
             if (!parser.VoidParse(response)) {
                 throw new ArgumentException("Network should have returned void");
             }
         }
 
-        public Position PlacePawn(Board board) {
+
+        public Position PlacePawn(Board board)
+        {
             XElement boardXml = toXml.PlacePawntoXml(board);
             string boardString = toXml.FormatXml(boardXml);
 
-            Console.WriteLine(boardString);
-
-            string response = Console.ReadLine();
+            string response = byteStreamHelper(boardString);
 
             Position pawnLocation = parser.PawnLocationParse(response);
             return pawnLocation;
         }
 
-        public Tile PlayTurn(Board board, List<Tile> hand, int unused) {
+
+        public Tile PlayTurn(Board board, List<Tile> hand, int unused)
+        {
             XElement playTurnXml = toXml.PlayTurntoXml(board, hand, unused);
             string playTurnString = toXml.FormatXml(playTurnXml);
 
-            Console.WriteLine(playTurnString);
-
-            string response = Console.ReadLine();
+            string response = byteStreamHelper(playTurnString);
 
             Tile playTile = parser.TileParse(response);
             return playTile;
         }
 
-        public void EndGame(Board board, List<string> colors) {
+
+        public void EndGame(Board board, List<string> colors)
+        {
             XElement endXml = toXml.EndGametoXml(board, colors);
             string endString = toXml.FormatXml(endXml);
 
-            Console.WriteLine(endString);
-
-            string response = Console.ReadLine();
+            string response = byteStreamHelper(endString);
 
             if (!parser.VoidParse(response))
             {
                 throw new ArgumentException("Network should have returned void");
             }
         }
+
+
+
+
+        // takes input string from the various functions
+        // returns output string 
+        // 
+        // converts string to bytes --> sends the bytes --> receives bytes back --> converts received bytes back to string
+        public string byteStreamHelper(string inputString)
+        {
+            byte[] readBuffer = new byte[1024];
+            byte[] writeBuffer = new byte[1024];
+
+            // send message
+            writeBuffer = Encoding.ASCII.GetBytes(inputString); // string to byte array
+            stream.Write(writeBuffer, 0, writeBuffer.Length);   // send byte array
+
+            // receive response
+            int numberOfBytesRead = 0;
+            StringBuilder completeMessage = new StringBuilder();
+            // Incoming message may be larger than the buffer size.
+            do
+            {
+                numberOfBytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
+
+                completeMessage.AppendFormat("{0}", Encoding.ASCII.GetString(readBuffer, 0, numberOfBytesRead));
+
+            }
+            while (stream.DataAvailable);
+            return completeMessage;
+        }
+
     }
 }
